82b3350304ff26a4de98b9a5ab90373157830921
ajball2001 <48214561+ajball2001@users.noreply.github.com>
2023-12-12 14:58:47 -0600

    Update main.cpp
    added pseudocode for dealer logic and for beginning of game checks

--- codefiles/main.cpp
+++ codefiles/main.cpp
@@ -19,10 +19,19 @@ int main()
           //deal player and check ace
           //deal dealer and check ace
       //reveal player
+    
       //reveal dealer second card
-      //while ask if player wants to draw
-          //deal player and check ace
-          //check for bust
-      //end 
+      //if dealer has blackjack and player has balckjack
+            //tie
+      //elif player has balckjack and dealer does not
+            //player wins
+      //elif dealer has blackjack and player does not
+            //dealer wins
+      //else
+          //while ask if player wants to draw
+              //deal player and check ace
+              //check for bust
+          //end 
+          //dealer draws if total is less than 16, stand on 17 or more
       
 }
