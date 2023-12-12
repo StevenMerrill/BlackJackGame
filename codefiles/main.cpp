c0c35e2ccac681bbe5d415cdea563ec2685ee1a0
StevenMerrill <152732396+StevenMerrill@users.noreply.github.com>
2023-12-12 14:55:21 -0600

    Update main.cpp
    PseudoCode

--- codefiles/main.cpp
+++ codefiles/main.cpp
@@ -7,10 +7,22 @@ using namespace std;
 
 
 
-
-
+//function check ace
+    // compares val and alt val
+    // if unequal returns true
 
 int main()
 {
-
+  //while loop to play game
+      //deal initial cards
+      // for 2
+          //deal player and check ace
+          //deal dealer and check ace
+      //reveal player
+      //reveal dealer second card
+      //while ask if player wants to draw
+          //deal player and check ace
+          //check for bust
+      //end 
+      
 }
